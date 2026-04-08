0beccde06f0969129446af474d3b0086570f6680
Prem <sah500prem@gmail.com>
2026-04-08 18:08:22 +0545

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dns from 'dns';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+console.log('Using DNS servers for MongoDB SRV lookup:', dns.getServers());
+
+mongoose.connect(process.env.MONGO, {
+  serverSelectionTimeoutMS: 10000,
+  connectTimeoutMS: 10000,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((error) => {
+  console.log('Error connecting to MongoDB:', error);
+});
 
 const app = express();
 
