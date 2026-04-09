3f659997f641593f30cf4174326d9d6d55396f45
Prem <sah500prem@gmail.com>
2026-04-09 10:22:18 +0545

    complete signup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'  // <--- add this
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],  // <--- include Tailwind plugin
+  plugins: [react()],
 })
